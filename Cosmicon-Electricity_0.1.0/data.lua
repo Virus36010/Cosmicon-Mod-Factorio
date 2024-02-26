2ca4613e0ff357ea4d220d8a4bcb4e7b690d6d2d
Virus <146997559+Virus36010@users.noreply.github.com>
2024-02-26 16:15:40 +0300

    Add files via upload

---
+++ Cosmicon-Electricity_0.1.0/data.lua
@@ -0,0 +1,7 @@
+
+local prt = "prototypes"
+
+require(prt.."item")
+require(prt.."recipe")
+require(prt.."technology")
+require(prt.."entity.entity")
---
+++ Cosmicon-Electricity_0.1.0/prototypes/item.lua
@@ -0,0 +1,65 @@
+
+local icon = "__Cosmicon-Electricity__/graphics/icons/"
+
+data:extend({
+  {-----------------------------LEP-4 1x1 18x18
+    type = "item",
+    name = "lep-4",
+    icon = icon.."lep-4.png",
+    icon_size=32,
+    scale = 0.5,
+    subgroup = "electricity",
+    order="db",
+    stack_size = 50,
+    place_result = "lep-4",
+  },
+
+  {-----------------------------LEP-5 2x2 64x64
+    type = "item",
+    name = "lep-5",
+    icon = icon.."lep-5.png",
+    icon_size=32,
+    scale = 0.5,
+    subgroup = "electricity",
+    order="ea",
+    stack_size = 50,
+    place_result = "lep-5",
+  },
+
+  {-----------------------------LEP-6 1x1 64x64
+    type = "item",
+    name = "lep-6",
+    icon = icon.."lep-6.png",
+    icon_size=32,
+    scale = 0.5,
+    subgroup = "electricity",
+    order="eb",
+    stack_size = 50,
+    place_result = "lep-6",
+  },
+  
+  {-----------------------------LEP-7 2x2 64x64 + 64x64
+    type = "item",
+    name = "lep-7",
+    icon = icon.."lep-7.png",
+    icon_size=32,
+    scale = 0.5,
+    subgroup = "electricity",
+    order="ec",
+    stack_size = 50,
+    place_result = "lep-7",
+  },
+
+  {-----------------------------LEP-8 1x1 64x64 + 64x64
+    type = "item",
+    name = "lep-8",
+    icon = icon.."lep-8.png",
+    icon_size=32,
+    scale = 0.5,
+    subgroup = "electricity",
+    order="ed",
+    stack_size = 50,
+    place_result = "lep-8",
+  },
+
+})
